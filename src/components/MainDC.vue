932d1f73a3b97e909b92938f0bfcdf0659c16e68
NicholasConti <conti94@live.it>
2023-03-23 16:18:12 +0100

    main prepared

--- src/components/MainDC.vue
+++ src/components/MainDC.vue
@@ -4,6 +4,26 @@ export default {
 }
 </script>
 <template>
-    Main
+    <div>
+        <main>
+            <h1>CONTENT HERE</h1>
+        </main>
+    </div>
 </template>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@use '../styles/general.scss' as *;
+
+div {
+    padding: 50px 0;
+    background-color: #1c1c1c;
+}
+
+main {
+    @include my-container;
+}
+
+h1 {
+    color: white;
+    font-size: 40px;
+}
+</style>
